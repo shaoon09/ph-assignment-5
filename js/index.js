a34897c9fa3c9a4796720ed4c2f0f6974ed9547b
shaoon09 <iiucclass1@gmail.com>
2024-09-25 08:37:28 +0600

    assignment Completed

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.getElementById('btn-first')
+document.getElementById('btn-one')
     .addEventListener('click', function (event) {
         event.preventDefault();
         const moneyAvailable = getTextFieldValueById('money-available');
@@ -9,6 +9,15 @@ document.getElementById('btn-first')
             document.getElementById('money-available').innerText = updatedMoneyAvailable;
             const updatedDonation = donatedFirst + donateMoneyFirst;
             document.getElementById('donated-amount-first').innerText = updatedDonation;
+            const div =document.createElement('div');
+            div.classList.add('border');
+            div.classList.add('rounded-lg');
+            div.classList.add('p-8');
+            div.classList.add('mb-5');
+            div.innerHTML = `
+            <h1 class="font-extrabold text-xl md:text-2xl mb-5">${donateMoneyFirst} Taka is Donated for Flood at Noakhali, Bangladesh</h1>
+            <p class="text-xl"> <span class="font-bold">Date & Time :</span> ${new Date().toLocaleString()}</p>`
+            document.getElementById('history-log').appendChild(div);
 
             document.getElementById('my_modal_1').showModal();
         }
@@ -17,7 +26,7 @@ document.getElementById('btn-first')
         }
     });
 
-document.getElementById('btn-second')
+document.getElementById('btn-two')
     .addEventListener('click', function (event) {
         event.preventDefault();
         const moneyAvailable = getTextFieldValueById('money-available');
@@ -28,15 +37,24 @@ document.getElementById('btn-second')
             document.getElementById('money-available').innerText = updatedMoneyAvailable;
             const updatedDonation = donatedSecond + donateMoneySecond;
             document.getElementById('donated-amount-second').innerText = updatedDonation;
+            const div =document.createElement('div');
+            div.classList.add('border');
+            div.classList.add('rounded-lg');
+            div.classList.add('p-8');
+            div.classList.add('mb-5');
+            div.innerHTML = `
+            <h1 class="font-extrabold text-xl md:text-2xl mb-5">${donateMoneySecond} Taka is Donated for Flood Relief in Feni,Bangladesh</h1>
+            <p class="text-xl"> <span class="font-bold">Date & Time :</span> ${new Date().toLocaleString()}</p>`
+            document.getElementById('history-log').appendChild(div);
 
             document.getElementById('my_modal_2').showModal();
         }
         else {
-            alert('Donation failed. Please enter a valid amount.');
+            alert('Thank you for your willingness to donate! Unfortunately, something went wrong. Could you please try again?');
         }
     });
 
-document.getElementById('btn-third')
+document.getElementById('btn-three')
     .addEventListener('click', function (event) {
         event.preventDefault();
         const moneyAvailable = getTextFieldValueById('money-available');
@@ -47,10 +65,19 @@ document.getElementById('btn-third')
             document.getElementById('money-available').innerText = updatedMoneyAvailable;
             const updatedDonation = donatedThird + donateMoneyThird;
             document.getElementById('donated-amount-third').innerText = updatedDonation;
+            const div =document.createElement('div');
+            div.classList.add('border');
+            div.classList.add('rounded-lg');
+            div.classList.add('p-8');
+            div.classList.add('mb-5');
+            div.innerHTML = `
+            <h1 class="font-extrabold text-xl md:text-2xl mb-5">${donateMoneyThird} Taka is Donated for Injured in the Quota Movement</h1>
+            <p class="text-xl"> <span class="font-bold">Date & Time :</span> ${new Date().toLocaleString()}</p>`
+            document.getElementById('history-log').appendChild(div);
 
             document.getElementById('my_modal_3').showModal();
         }
         else {
-            alert('Donation failed. Please enter a valid amount.');
+            alert('Thank you for your willingness to donate! Unfortunately, something went wrong. Could you please try again?');
         }
     });
--- js/utilities.js
+++ js/utilities.js
@@ -9,3 +9,15 @@ function getTextFieldValueById(id) {
     const textNumber = parseFloat(textValue);
     return textNumber;
 }
+
+function showSectionById(id) {
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+}
+
+function changeColorById(id) {
+    document.getElementById('donation-btn').classList.add('btn-outline');
+    document.getElementById('history-btn').classList.add('btn-outline');
+    document.getElementById(id).classList.remove('btn-outline');
+}
